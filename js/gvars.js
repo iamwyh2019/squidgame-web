ebe89fb92c60ec8f0be2beeec9b0925d885c1c6d
yuhengwu <799810767@qq.com>
2022-02-01 19:33:59 +0800

    Initial commit

---
+++ js/gvars.js
@@ -0,0 +1,34 @@
+// window size
+const c_width = 1080
+const c_height = 608
+
+// frequently used colors
+const WHITE = '#ffffff'
+const RED = '#ff0000'
+const GREEN = '#00ff00'
+const BLUE = '#0000ff'
+const BLACK = '#000000'
+const GRAY = '#808080'
+const LIGHTGRAY = '#c0c0c0'
+const BABYBLUE = '#92d7ef'
+
+// main page
+const c_header_width = c_width / 2
+const c_header_height = c_height / 3
+const c_instr_width = c_width / 3
+const c_instr_height = c_instr_width / 8
+
+// cat
+const c_cat_width = c_width / 18
+const c_cat_height = c_cat_width
+
+// Red line coordinate, dimensions, and color
+const c_redline_left = c_width / 6
+const c_redline_top = 0
+const c_redline_width = 5
+const c_redline_height = c_height
+const c_redline_color = BLACK
+
+// player
+const c_player_width = c_width / 15
+const c_player_height = c_player_width
---
+++ readme.md
@@ -0,0 +1,27 @@
+A small present!
+
+The web version of *SquidGame in CovidLife*.
+
+
+
+### Run the game
+
+#### Run on a server
+
+Just upload everything to the server and access with url.
+
+#### Run locally
+
+First, make sure you have two things installed:
+
+1. **node.js**: download from https://nodejs.org/en/
+
+2. **Anywhere**: after node.js is installed, open a terminal, type
+
+   ```shell
+   npm install anywhere
+   ```
+
+You only need to install these two things once.
+
+Then, open a terminal at this folder, and type `anywhere`. A browser tab should pop up with the game.
